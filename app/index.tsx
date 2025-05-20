3f004090e7acf93c4c7489786eaf5b0c3cf4d55a
Luiz Cenci <155825996+LuizCenci@users.noreply.github.com>
2025-05-19 22:14:40 -0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -10,11 +10,7 @@ export default function index(){
             <Button title='filter' onPress={() => router.push('/filter')}/>
             <Button title='login' onPress={() => router.push('/login')}/>
             <Button title='cadastro-servico' onPress={() => router.push('/cadastro-servico')}/>
-<<<<<<< HEAD
-            <Button title='avaliacao' onPress={() => router.push('/avaliacao')}/>
-=======
             <Button title='avaliar' onPress={() => router.push('/avaliacao')}/>
->>>>>>> 3b47a523a2374b1b8d5bfdb63ad6bbd3779a1f9b
         </View>
     );
     
