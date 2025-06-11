f4c818d6d5d3f3e6d6c62250bfc6d4a4f349d8df
LucasBonii <158228644+LucasBonii@users.noreply.github.com>
2025-06-10 23:26:18 -0300

    login -> landingpage

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,6 @@
-import { useRouter } from 'expo-router';
+import { Redirect } from 'expo-router';
 import React from 'react';
-import { Button, View } from 'react-native';
 
-export default function index(){
-    const router = useRouter();
-    return(
-        <View>
-            <Button title='cadastro' onPress={() => router.push('/cadastro')}/>
-            <Button title='filter' onPress={() => router.push('/filter')}/>
-            <Button title='login' onPress={() => router.push('/login')}/>
-            <Button title='cadastro-servico' onPress={() => router.push('/cadastro-servico')}/>
-            <Button title='agendamento' onPress={() => router.push('/agendamento')}/>
-            <Button title='avaliar' onPress={() => router.push('/avaliacao')}/>
-            <Button title='dashboard' onPress={() => router.push('/dashboard')}/>
-            <Button title='dashboardMudanceiro' onPress={() => router.push('/dashboardMudanceiro')}/>
-        </View>
-    );
-    
+export default function Index() {
+  return <Redirect href="/login" />;
 }
